eca039dda64dc1f273f365ac530214fe9dc4c03a
jsa7868 <chhak0503@gmail.com>
2024-02-26 17:30:57 +0900

    jboard2

---
+++ jboard2/src/main/java/kr/co/jboard2/controller/user/FindController.java
@@ -0,0 +1,37 @@
+package kr.co.jboard2.controller.user;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/user/findId.do")
+public class FindController extends HttpServlet {
+
+	private static final long serialVersionUID = 6747872538849006388L;
+	
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+	}
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		RequestDispatcher dispatcher=req.getRequestDispatcher("/user/findIdjsp");
+		dispatcher.forward(req, resp);
+	
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		super.doPost(req, resp);
+	}
+
+	
+	
+}
---
+++ jboard2/src/main/webapp/_footer.jsp
@@ -0,0 +1,9 @@
+<%@ page contentType="text/html;charset=UTF-8" pageEncoding="UTF-8"%>
+<html lang="en">
+
+        <footer>
+            <p>ⓒCopyright jsa7868.or.kr</p>
+        </footer>
+    </div>    
+</body>
+</html>
